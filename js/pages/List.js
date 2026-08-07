2d13edb07c1fd746e98a1029c110c106b2453741
freyaii <jorge0412aguilar@gmail.com>
2026-08-07 13:08:32 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -52,7 +52,7 @@ export default {
                         </li>
                         <li>
                             <div class="type-title-sm">FPS</div>
-                            <p>{{ level.fps }}</p>
+                            <p>{{ level.fps || 'Any' }}</p>
                         </li>
                         <li>
                             <div class="type-title-sm">Cps Cap</div>

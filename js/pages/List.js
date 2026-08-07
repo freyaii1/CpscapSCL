f46b4c8139d34cb970b4f6ebce0b55641e649d98
freyaii <jorge0412aguilar@gmail.com>
2026-08-06 21:45:55 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -128,7 +128,7 @@ export default {
                         Do not use easy modes, only a record of the unmodified level qualifies
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Once a level falls onto the Legacy List, records can not be added to it anymore
                     </p>
                 </div>
             </div>

98df00d78e1a237ea11e2bc00022ffc6f1be48c4
freyaii <jorge0412aguilar@gmail.com>
2026-08-06 21:03:00 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -51,8 +51,8 @@ export default {
                             <p>{{ level.id }}</p>
                         </li>
                         <li>
-                            <div class="type-title-sm">Password</div>
-                            <p>{{ level.password || 'Free to Copy' }}</p>
+                            <div class="type-title-sm">Cps Cap</div>
+                            <p>{{ level.cap || 'Free to Copy' }}</p>
                         </li>
                     </ul>
                     <h2>Records</h2>
